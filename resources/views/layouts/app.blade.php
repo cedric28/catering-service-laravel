9516e4327d04b854ae3bd591ab86bc9a3a376578
cedriclegarda97 <cedric.legarda97@gmail.com>
2022-09-12 20:51:04 +0800

    fontawesome remove secure assets

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -21,7 +21,7 @@
 	<link rel="icon" href="{{ secure_asset('/assets/img/logo-pink.ico') }}">
 	<link rel="shortcut icon" type="image/x-icon" href="{{ secure_asset('/assets/img/logo-pin.ico') }}">
 	<!-- Global stylesheets -->
-	<link href="{{ secure_asset('/assets/vendor/fontawesome-free/css/all.min.css') }}" rel="stylesheet" type="text/css">
+	<link href="{{ asset('assets/vendor/fontawesome-free/css/all.min.css') }}" rel="stylesheet" type="text/css">
 	<link
 	href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
 	rel="stylesheet">
