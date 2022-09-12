ac0efb10caa746a6d136e40c986ca1df34e1aee3
cedriclegarda97 <cedric.legarda97@gmail.com>
2022-09-12 18:28:59 +0800

    update appservice provider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -29,38 +29,45 @@ class AppServiceProvider extends ServiceProvider
      * @return void
      */
     public function boot()
-    {   
-        View::composer(['*','partials.header'], function ($view) {
-            if(Auth::check()){
+    {
+        if (\App::environment([
+            'staging',
+            'production',
+        ])) {
+            \URL::forceScheme('https');
+        }
+
+        View::composer(['*', 'partials.header'], function ($view) {
+            if (Auth::check()) {
                 $job_type_id = auth()->user()->job_type_id;
-                if($job_type_id == 2){  
+                if ($job_type_id == 2) {
                     $notification = TaskStaffNotification::where([
-                        ['user_id','=',\Auth::user()->id],
-                        ['is_read','=',0]
+                        ['user_id', '=', \Auth::user()->id],
+                        ['is_read', '=', 0]
                     ])->get();
                     $view->with('notificationStaff', $notification)
-                    ->with('notificationStaffCount', count($notification));
-                } else if($job_type_id == 3 || $job_type_id == 4 || $job_type_id == 5) {
+                        ->with('notificationStaffCount', count($notification));
+                } else if ($job_type_id == 3 || $job_type_id == 4 || $job_type_id == 5) {
                     $notification = TaskNotification::where([
-                        ['user_id','=',\Auth::user()->id],
-                        ['is_read','=',0]
+                        ['user_id', '=', \Auth::user()->id],
+                        ['is_read', '=', 0]
                     ])->get();
                     $view->with('notificationStaffing', $notification)
-                    ->with('notificationStaffingCount', count($notification));
+                        ->with('notificationStaffingCount', count($notification));
                 }
             }
         });
-          
+
         Schema::defaultStringLength(191);
 
         Str::macro('currency', function ($price) {
-            return 'P'.number_format($price, 2);
+            return 'P' . number_format($price, 2);
         });
 
         Str::macro('number_comma', function ($number) {
             return number_format($number);
         });
-        
+
         Str::macro('date_task_format', function ($task_date) {
             return Carbon::parse($task_date)->format('F d Y');
         });
